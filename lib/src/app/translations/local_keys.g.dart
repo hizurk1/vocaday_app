1b55afc79eac2edcde826e4238ea3bf812cf9c44
Helkaloic <huuhieu992001@gmail.com>
2023-12-30 22:59:57 +0700

    refactor

---
+++ lib/src/app/translations/local_keys.g.dart
@@ -0,0 +1,46 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const on_board_select_language = 'on_board.select_language';
+  static const on_board_select_language_content = 'on_board.select_language_content';
+  static const on_board_onboard_title = 'on_board.onboard_title';
+  static const on_board_onboard_body = 'on_board.onboard_body';
+  static const on_board_get_started = 'on_board.get_started';
+  static const app_language_english = 'app_language.english';
+  static const app_language_vietnamese = 'app_language.vietnamese';
+  static const common_next = 'common.next';
+  static const common_accept = 'common.accept';
+  static const common_cancel = 'common.cancel';
+  static const common_skip = 'common.skip';
+  static const common_done = 'common.done';
+  static const common_browse = 'common.browse';
+  static const common_other_k = 'common.other_k';
+  static const auth_already_have_an_account = 'auth.already_have_an_account';
+  static const auth_sign_in = 'auth.sign_in';
+  static const auth_sign_up = 'auth.sign_up';
+  static const auth_welcome = 'auth.welcome';
+  static const auth_enter_email = 'auth.enter_email';
+  static const auth_enter_password = 'auth.enter_password';
+  static const auth_forgot_password = 'auth.forgot_password';
+  static const auth_or_sign_in_with = 'auth.or_sign_in_with';
+  static const auth_create_an_account = 'auth.create_an_account';
+  static const auth_confirm_password = 'auth.confirm_password';
+  static const auth_please_enter_and_password = 'auth.please_enter_and_password';
+  static const auth_password_does_not_match = 'auth.password_does_not_match';
+  static const auth_sign_in_as_email = 'auth.sign_in_as_email';
+  static const auth_invalid_email = 'auth.invalid_email';
+  static const auth_invalid_password = 'auth.invalid_password';
+  static const auth_sign_out = 'auth.sign_out';
+  static const app_log_on_restart = 'app_log.on_restart';
+  static const user_data_point = 'user_data.point';
+  static const page_home = 'page.home';
+  static const page_search = 'page.search';
+  static const page_activity = 'page.activity';
+  static const page_profile = 'page.profile';
+  static const page_setting = 'page.setting';
+  static const search_search_for_words = 'search.search_for_words';
+  static const search_type_something = 'search.type_something';
+  static const search_not_found = 'search.not_found';
+  static const search_are_you_looking_for = 'search.are_you_looking_for';
+
+}
