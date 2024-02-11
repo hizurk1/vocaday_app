458b2fad5700ef8668acd724049e09c3ab5da960
Helkaloic <huuhieu992001@gmail.com>
2024-02-11 09:46:22 +0700

    [doc] add doc for Validator class [test] add test cases for Validator class

--- lib/app/utils/validator.dart
+++ lib/app/utils/validator.dart
@@ -5,32 +5,44 @@ class Validator {
   ///
   /// The function returns `true` if the email address is valid, and `false` if it is not.
   ///
-  /// The function uses a regular expression to match the email address against a specific format.
+  /// The pattern matches strings that follow the format of `"username@domain.com"`,
+  /// where the username can contain alphanumeric characters and underscores,
+  /// and the domain can contain alphanumeric characters, hyphens, and dots.
   static bool validateEmail(String email) {
-    /// A regular expression pattern to match an email address.
     const String pattern =
         r'^[a-zA-Z0-9_]+(?:\.[a-zA-Z0-9_]+)*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$';
 
-    /// A regular expression object that matches the email address pattern.
-    final RegExp regex = RegExp(pattern);
-
-    /// Returns `true` if the email address matches the pattern, and `false` if it does not.
-    return regex.hasMatch(email);
+    return RegExp(pattern).hasMatch(email);
   }
 
   /// A function to validate a password.
   ///
   /// The function returns `true` if the password is valid, and `false` if it is not.
   ///
-  /// The function uses a regular expression to match the password against a specific format.
+  /// Pattern means that the password should contains:
+  ///
+  /// * `(?=.*[a-zA-Z])`: at least one alphabetical character.
+  /// * `(?=.*\d)`: at least one digit.
+  /// * `[a-zA-Z\d]{8,}`: at least 8 characters. These characters can be a combination of uppercase letters, lowercase letters, and digits.
   static bool validatePassword(String password) {
-    /// A regular expression pattern to match a password.
     const String pattern = r'^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$';
 
-    /// A regular expression object that matches the password pattern.
-    final RegExp regex = RegExp(pattern);
+    return RegExp(pattern).hasMatch(password);
+  }
 
-    /// Returns `true` if the password matches the pattern, and `false` if it does not.
-    return regex.hasMatch(password);
+  /// A function to validate a phone number.
+  ///
+  /// The function returns `true` if the phoneNumber is valid, and `false` if it is not.
+  ///
+  /// The pattern accepts international phone numbers that start with a `+` or `0` followed by 6 to 10 digits.
+  ///
+  /// Examples of valid strings that match this pattern:
+  /// * +1234567890
+  /// * 0123456789
+  /// * +44987654
+  static bool validatePhoneNumber(String phoneNumber) {
+    const String pattern = r'^(\+[0-9]{1,2}|0)+[0-9]{6,10}$';
+
+    return RegExp(pattern).hasMatch(phoneNumber);
   }
 }
--- test/app/utils/validator_test.dart
+++ test/app/utils/validator_test.dart
@@ -25,4 +25,31 @@ void main() {
       expect(Validator.validateEmail("test_.#\$!-=+'\"|/\\@test.com"), false);
     });
   });
+  group("Password validate", () {
+    test("should get [true] is a valid password", () {
+      expect(Validator.validatePassword("abc12345"), true);
+    });
+    test("should get [false] is a invalid password", () {
+      expect(Validator.validatePassword(""), false);
+      expect(Validator.validatePassword("12345"), false);
+      expect(Validator.validatePassword("123456789"), false);
+      expect(Validator.validatePassword("abcdefgh"), false);
+    });
+  });
+
+  group("Phone number validate", () {
+    test("should get [true] is a valid phone number", () {
+      expect(Validator.validatePhoneNumber("0987654321"), true);
+      expect(Validator.validatePhoneNumber("0654321"), true);
+      expect(Validator.validatePhoneNumber("+34876543210"), true);
+      expect(Validator.validatePhoneNumber("+1654321"), true);
+    });
+    test("should get [false] is a invalid phone number", () {
+      expect(Validator.validatePhoneNumber(""), false);
+      expect(Validator.validatePhoneNumber("123456789"), false);
+      expect(Validator.validatePhoneNumber("1234567890123"), false);
+      expect(Validator.validatePhoneNumber("+01"), false);
+      expect(Validator.validatePhoneNumber("+123456"), false);
+    });
+  });
 }
